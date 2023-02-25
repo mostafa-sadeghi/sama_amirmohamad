a01010035668af4c68663bc1ecb5cbd723d15169
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-25 17:00:27 +0330

    first commit

--- s1.py
+++ s1.py
@@ -15,3 +15,11 @@ score = score + 1
 print("new score is:", score)
 score = score - 1
 print("new score is:", score)
+
+# exercise 1:
+# با کمک پرینت،نام و نام خانوادگی و سن خودتان را پرینت نمائید.
+# exercise 2 :
+# با کمک پرینت حاصل عبارات زیر را نمایش دهید:
+# 1 + 2 + 3
+# 2 * 2 - 1
+# 2 + 2 - 2
