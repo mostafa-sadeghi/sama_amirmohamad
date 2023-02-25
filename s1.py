6054c39cce40034052e215cb7f88b9870451f25d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-25 16:52:23 +0330

    first commit

---
+++ s1.py
@@ -0,0 +1,17 @@
+# print('Hello world')
+# print("1+2")
+# print(1 + 2)
+# print()
+# print("welcome to our python class.")
+
+score = 0
+print("score:", score)
+print("new score is:", score + 1)
+print("new score is:", "score + 1")
+print("new score is:", score - 1)
+
+print("score:", score)
+score = score + 1
+print("new score is:", score)
+score = score - 1
+print("new score is:", score)
