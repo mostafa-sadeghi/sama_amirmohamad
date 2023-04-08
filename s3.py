6b5895f449c023b4f943793a14bbdb5a0c6e823a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-08 16:59:20 +0330

    sadeghi

--- s3.py
+++ s3.py
@@ -16,16 +16,29 @@
 # print(int(number1) + int(number2))
 
 
-name = input('enter your name: ')
-age = input('enter your age: ')
-message = '%s has %s years old' % (name, age)
-print(message)
+# name = input('enter your name: ')
+# age = input('enter your age: ')
+# message = '%s has %s years old' % (name, age)
+# print(message)
 
 # ex1
 # برنامه ای بنویسید که نام، نام خانوادگی  و نام پدرتان از ورودی دریافت نماید و
 # پیغامی ماشبه زیر نمایش دهد:
 # sama jokar's father's name is mohammad
 
+name = input('enter your name: ')
+family = input('enter your family: ')
+father_name = input("enter your father's name ")
+print(name + " " + family + "'s " + "father name is " + father_name)
+
+print("%s %s's father name is %s"%(name, family, father_name))
+
+print(f"{name} {family}'s father name is {father_name}")
+
 # ex2 برنامه ای بنویسید که سه عدد را از ووردی در یافت نماید و
 # حاصل عبارت زیر را محاسبه و نمایش دهد
 # عدد اول - عدد دوم + عدد سوم
+n1 = int(input('enter a number: '))
+n2 = int(input('enter a number: '))
+n3 = int(input('enter a number: '))
+print(n3 + n2 - n1)
---
+++ s4.py
@@ -0,0 +1,67 @@
+# کامنت
+
+# print("hi")
+
+# x = 12
+
+# x = 'nikan'
+
+# print("x=",x)
+
+
+# score = 20
+
+# score = score + 1
+
+# print("score=", score)
+
+# print(5/2)
+# print(5//2)
+
+# name = input('enter student name: ')
+# score1 = float(input('enter first score: '))
+# score2 = float(input('enter second score: '))
+# score3 = float(input('enter third score: '))
+
+# average = (score1 + score2 + score3) / 3
+# print(f"{name} average is : {average}")
+
+
+# a = 4
+# b = 5
+
+# if a > b :
+#     print("a is greater than b.")
+
+# if a < b :
+#     print("a is less than b.")
+
+# a = 4
+# b = 4
+
+# if a >= b:
+#     print("a is greater than or equal to b.")
+
+# if a <= b:
+#     print("a is less tha nor equal to b.")
+
+
+# تمرین اول
+'''
+برنامه ای بنویسد که نمره درس برنامه نویسی علی را از ورودی دریافت نماید و اگر نمره
+از 18 بالاتر بود 
+A 
+را پرینت نماید
+اگر از 18 پائین تر بود
+B 
+را پرینت نماید
+
+'''
+# تمرین دوم
+# برنامه ای بنویسید که نمره سه درس دانش آموزی را از ورودی دریافت نماید
+# و معدل دانش آموز را محاسبه کند
+# در انتها اگر معدل او از
+# 19
+# بالاتر بود
+# good
+# را پرینت نماید
