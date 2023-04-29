7db28fa1b00f6ec4f10e12954a76da2ec4a3bcee
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-29 16:59:47 +0330

    sadeghi

---
+++ s6.py
@@ -0,0 +1,48 @@
+# import turtle
+
+# s = turtle.Screen()
+# p = turtle.Pen()
+# p.shape('turtle')
+# p.pencolor('red')
+# p.pensize(2)
+# p.fillcolor('green')
+# p.begin_fill()
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+
+# p.forward(100)
+# p.left(90)
+# p.end_fill()
+
+# s.exitonclick()
+
+# کشیدن مثلث
+# کشیدن پنجضلعی
+# کشیدن ششضلعی
+
+for i in range(6):
+    print("hi")
+    print("another")
+
+print("outside")
+
+import turtle 
+s = turtle.Screen()
+p = turtle.Pen()
+
+for i in range(4):
+    p.forward(100)
+    p.left(90)
+
+s.exitonclick()
+
+# با کمک حلقه فور
+# کشیدن مثلث
+# کشیدن پنجضلعی
+# کشیدن ششضلعی
